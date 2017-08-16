f1158f9c3f06d88fcf53dc1486c1732bb694c780
s0up <mike@haxt.net>
2017-08-16 14:45:44 -0700

    test

---
+++ src/renderer/components/Common/WindowTop.vue
@@ -0,0 +1,31 @@
+<template>
+<div class='window-top text-right' style="-webkit-app-region: drag">
+  <span @click="minimize" class="glyphicon glyphicon-chevron-down"></span>
+  <span @click="maximize" class="glyphicon glyphicon-new-window"></span>
+  <span @click="close" class="glyphicon glyphicon-remove"></span>
+</div>
+</template>
+<script>
+const remote = require('electron').remote;
+
+export default {
+  methods: {
+    minimize() {
+      let window = remote.getCurrentWindow();
+      window.minimize();
+    },
+    maximize() {
+      let window = remote.getCurrentWindow();
+      if (!window.isMaximized()) {
+        window.maximize();
+      } else {
+        window.unmaximize();
+      }
+    },
+    close(){
+      let window = remote.getCurrentWindow();
+      window.close();
+    }
+  }
+}
+</script>
